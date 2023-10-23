f8f29789395bb20c189f1bee539939f0645d4919
Pascal Michael Bayer <pascal.bayer1999@gmail.com>
2023-10-23 15:46:08 +0200

    initial commit

---
+++ src/main/java/org/eventManagement/user.java
@@ -0,0 +1,45 @@
+package org.eventManagement;
+
+public class user {
+
+    private String email;
+    private String name;
+    private String id;
+    private String password;
+
+    public user() {
+    };
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
